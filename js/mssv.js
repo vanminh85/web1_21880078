40fa0e99742b29cfeaa0f82760cb8de5676b2401
vanminh85 <minh.nguyen@katalon.com>
2023-10-18 01:49:43 +0700

    apply handlebars

---
+++ js/mssv.js
@@ -0,0 +1,12 @@
+const API='https://web1-api.vercel.app/api';
+
+async function loadData(request, templateId, viewId){
+    const response = await fetch(`${API}/${request}`);
+    const data = await response.json();
+
+    var source = document.getElementById(templateId).innerHTML;
+    var template = Handlebars.compile(source);
+    var context = { data : data };
+    var view = document.getElementById(viewId);
+    view.innerHTML = template(context);
+  }
