4d13f2d10af00460a9502384d389457930b7c820
vanminh85 <minh.nguyen@katalon.com>
2023-10-23 10:04:20 +0700

    update

--- js/mssv.js
+++ js/mssv.js
@@ -1,6 +1,6 @@
 const API='https://web1-api.vercel.app/api';
 
-async function loadData(request, templateId, viewId){
+async function loadData(request, templateId, viewId) {
     const response = await fetch(`${API}/${request}`);
     const data = await response.json();
 
